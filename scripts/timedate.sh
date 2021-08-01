fdbf9af00de0140c40cf390077cf64f87c4279cb
Chris5692 <52839899+Chris5692@users.noreply.github.com>
2021-08-01 07:54:33 +0530

    updated the timedate script

--- scripts/timedate.sh
+++ scripts/timedate.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#date | awk '{print $3" "$2" "$1}'			## Print date 
-date | awk '{print $5" "$6}'				## Print time
-#date | awk '{print $3" "$2" "$1" "$5" "$6}'		## Print time and date
+date %I:%M%t%p | awk '{print $1"  "$2}'
